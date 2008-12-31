a4ccf61dc462792d81f25ab3e8f3744a53b620cc
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2008-12-31 01:56:33 +0000

    Revised (now backwards-compatible) polymorphic port extension (result of #lv2 conversion with kfoltman).
    git-svn-id: http://svn.drobilla.net/lad@1916 a436a847-0d15-0410-975c-d299462d15a1

--- trunk/lv2/ext/polymorphic_port/lv2_polymorphic_port.h
+++ trunk/lv2/ext/polymorphic_port/lv2_polymorphic_port.h
@@ -19,12 +19,12 @@
 #ifndef LV2_POLYMORPHIC_PORT_H
 #define LV2_POLYMORPHIC_PORT_H
 
-#define LV2_POLYMORPHIC_PORT_URI "http://lv2plug.in/ns/ext/polymorphic-port"
+#define LV2_POLYMORPHIC_PORT_URI "http://lv2plug.in/ns/dev/polymorphic-port"
 
 
 /** @file
  * This header defines the LV2 Polymorphic Port extension with the URI
- * <http://lv2plug.in/ns/ext/polymorphic-port>.
+ * <http://lv2plug.in/ns/dev/polymorphic-port>.
  *
  * This extension defines a buffer format for ports that can take on
  * various types dynamically at runtime.
@@ -34,22 +34,31 @@
 /** The data field of the LV2_Feature for this extension.
  *
  * To support this feature the host must pass an LV2_Feature struct to the
- * instantiate method with URI "http://lv2plug.in/ns/ext/polymorphic-port"
+ * instantiate method with URI "http://lv2plug.in/ns/dev/polymorphic-port"
  * and data pointed to an instance of this struct.
  */
 typedef struct {
 	
-	/** A URI mapped to an integer with the LV2 URI map extension
-	 * <http://lv2plug.in/ns/ext/uri-map> which describes the type of
-	 * data pointed to by buf.
+	/** Set the type of a polymorphic port.
+	 * If the plugin specifies constraints on port types, the host MUST NOT
+	 * call the run method until all port types have been set to a valid
+	 * configuration.  Whenever the type for a port is changed, the host
+	 * MUST call connect_port before the next call to the run method.
+	 * The return value of this function SHOULD be ignored by hosts at this
+	 * time (future revisions of this extension may specify return values).
+	 * Plugins which do not know of any future revision or extension that
+	 * dictates otherwise MUST return 0 from this function.
+	 * @param port Index of the port to connect (same as LV2 connect_port)
+	 * @param type Mapped URI for the type of data being connected
+	 * @param type_data Type specific data defined by type URI (may be NULL)
+	 * @return Unused at this time
 	 */
-	uint32_t type;
-
-	/** Pointer to the actual port contents.
-	 */
-	void* buf;
-
-} LV2_Polymorphic_Buffer;
+	uint32_t (*set_type)(LV2_Handle instance,
+	                     uint32_t   port,
+	                     uint32_t   type,
+	                     void*      type_data);
+	
+} LV2_Polymorphic_Feature;
 
 
 #endif // LV2_POLYMORPHIC_PORT_H
