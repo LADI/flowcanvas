5cdacdc328afed2887d8e95522f47e0763ae33ae
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2009-07-05 02:44:59 +0000

    Generate documentation for everything possible.
    git-svn-id: http://svn.drobilla.net/lad@2150 a436a847-0d15-0410-975c-d299462d15a1

--- trunk/lv2/gendoc.sh
+++ trunk/lv2/gendoc.sh
@@ -2,13 +2,20 @@
 
 cd ext
 
+echo
+echo "**** Generating code documentation with doxygen"
 doxygen Doxyfile
 
-for b in dyn-manifest port-groups uri-map; do
-	if [ -x $b.lv2/$b.ttl ]; then
+for bundle in `find -name '*.lv2'`; do
+	b=`echo $bundle | sed 's/\.lv2$//' | sed 's/^.*\///'`
+	if [ -e $b.lv2/$b.ttl ]; then
+		echo
+		echo "**** Generating RDF documentation $b.html"
 		../specgen/lv2specgen.py $b.lv2/$b.ttl dman ../specgen/template.html $b.lv2/$b.html -i;
 	fi
-	if [ -x $b.lv2/$b.h ]; then
+	if [ -e $b.lv2/$b.h ]; then
+		echo
+		echo "**** Copying code documentation $b.h.html"
 		cp doc/html/${b}_8h.html $b.lv2/$b.h.html;
 	fi
 done
