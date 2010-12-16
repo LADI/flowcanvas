51287b21cdac9f09fabb48ef3c2df9e154b34184
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2010-12-16 04:04:45 +0000

    Updates for waf 1.6. Note: this revision breaks documentation installation.
    git-svn-id: http://svn.drobilla.net/lad@2719 a436a847-0d15-0410-975c-d299462d15a1

--- trunk/maintain.mk
+++ trunk/maintain.mk
@@ -2,10 +2,10 @@ all:
 	@echo "Targets: upload-docs"
 
 upload-docs:
-	rsync -avz --delete -e ssh build/default/flowcanvas/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/flowcanvas
-	rsync -avz --delete -e ssh build/default/raul/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/raul
-	rsync -avz --delete -e ssh build/default/redlandmm/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/redlandmm
-	rsync -avz --delete -e ssh build/default/slv2/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/slv2
+	rsync -avz --delete -e ssh build/flowcanvas/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/flowcanvas
+	rsync -avz --delete -e ssh build/raul/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/raul
+	rsync -avz --delete -e ssh build/redlandmm/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/redlandmm
+	rsync -avz --delete -e ssh build/slv2/doc/html/* drobilla@drobilla.net:~/drobilla.net/docs/slv2
 
 # Doesn't work
 deb:
