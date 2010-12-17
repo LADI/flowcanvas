053da458247fa4c6ea66f82e536d73cce2011097
drobilla <drobilla@a436a847-0d15-0410-975c-d299462d15a1>
2010-12-17 19:12:44 +0000

    Remove World.cpp.
    git-svn-id: http://svn.drobilla.net/lad@2750 a436a847-0d15-0410-975c-d299462d15a1

--- trunk/redlandmm/redlandmm/World.hpp
+++ trunk/redlandmm/redlandmm/World.hpp
@@ -38,14 +38,45 @@ namespace Redland {
  */
 class World : public boost::noncopyable, public Wrapper<librdf_world> {
 public:
-	World();
-	~World();
+	inline World()
+		: _next_blank_id(0)
+	{
+		if (!Glib::thread_supported())
+			Glib::thread_init();
+		
+		_mutex = new Glib::Mutex();
+		
+		_c_obj = librdf_new_world();
+		librdf_world_open(_c_obj);
+		
+		add_prefix("rdf", "http://www.w3.org/1999/02/22-rdf-syntax-ns#");
+	}
+
+	inline ~World() {
+		Glib::Mutex::Lock lock(*_mutex);
+		librdf_free_world(_c_obj);
+	}
 	
 	inline uint64_t blank_id() { return _next_blank_id++; }
 
-	void        add_prefix(const std::string& prefix, const std::string& uri);
-	std::string expand_uri(const std::string& uri) const;
-	std::string qualify(const std::string& uri) const;
+	void add_prefix(const std::string& prefix, const std::string& uri) {
+		_prefixes[prefix] = uri;
+	}
+
+	std::string expand_uri(const std::string& uri) const {
+		if (uri.find(":") == std::string::npos)
+			return uri;
+
+		for (Namespaces::const_iterator i = _prefixes.begin(); i != _prefixes.end(); ++i)
+			if (uri.substr(0, i->first.length()+1) == i->first + ":")
+				return i->second + uri.substr(i->first.length()+1);
+
+		return uri;
+	}
+	
+	std::string qualify(const std::string& uri) const {
+		return _prefixes.qualify(uri);
+	}
 
 	const Namespaces& prefixes() const { return _prefixes; }
 
